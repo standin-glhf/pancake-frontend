9333057e780057795288084f5b1245742b4f97d4
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-02-10 13:11:22 +0800

    fix(prediction): Prevent crash when current round not found

--- src/views/Predictions/components/Label.tsx
+++ src/views/Predictions/components/Label.tsx
@@ -131,6 +131,10 @@ export const TimerLabel: React.FC<TimerLabelProps> = ({ interval, unit }) => {
   const countdown = formatRoundTime(secondsRemaining)
   const { t } = useTranslation()
 
+  if (!currentRoundLockTimestamp) {
+    return null
+  }
+
   return (
     <Box pr="24px" position="relative">
       <Label dir="right">
